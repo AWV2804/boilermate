b6881a0c9d080ab432d05f76ae650b85ecd6c722
Matthew Li <mli007@hawaiiantel.net>
2024-02-01 16:26:55 -0500

    incorporate beautiful soup

--- boilermate_project/boilermate_app/urls.py
+++ boilermate_project/boilermate_app/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ClassViewSet
+from .views import ScrapeView
 from .urls import home
 
 router = DefaultRouter()
@@ -9,6 +10,7 @@ router.register(r'Classes', ClassViewSet, basename='classModel')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('scrape/', ScrapeView.as_view(), name='scrape_view'),
     path('', home, name='home'),
     path('', include(router.urls)),
 ]
--- boilermate_project/boilermate_app/views.py
+++ boilermate_project/boilermate_app/views.py
@@ -2,6 +2,29 @@
 from rest_framework import viewsets
 from .models import Classes
 from .serializers import ClassesSerializer
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+import requests
+from django.views import View
+
+class ScrapeView(View):
+    template_name = 'scrape_result.html'
+    
+    def get(self, request):
+        url = 'https://putsomethinghere.com' # replace with url we want to scrape
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            name = soup.title.string
+            topics = ' '.join([p.text for p in soup.find_all('p')])
+            
+            new_class = Classes.objects.create(name=name, topics=topics)
+            new_class.save() #saves content to the mysql database
+            
+            return HttpResponse(f"Scraped Class: {name} saved to database")
+        else:
+            return HttpResponse("Failed to fetch the webpage")
 
 class ClassViewSet(viewsets.ModelViewSet):
     queryset = Classes.objects.all()
