8e384d61cc338043cc7d94f3d71ad0b9c04c27de
Andrew Tu <andrewtu517@gmail.com>
2024-02-01 18:27:25 -0500

    Completed Web Scraping Script for topics
    the python script now has a function that can be called which returns a dictionary with the keys being the names of the classes and the values being a list of the topics

--- web_scrape.py
+++ web_scrape.py
@@ -1,23 +1,75 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL to scrape
-url = "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/courseInfo?courseid=716&show=true&type=undergrad"
 
-# Send a GET request to the URL
-response = requests.get(url)
+def get_topics():
+    # hard coding part of the url
+    dynamicCourseUrl = "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/"
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the content of the page with BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
+    # list of classes page to scrape
+    classesUrl = "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/coursesUndergrad"
 
-    # This shows most of the stuff I think
-    #print(soup.prettify())
+    # Send a GET request to the URL
+    classResponse = requests.get(classesUrl)
 
     # Gets some of the schedule tags
-    topics = soup.find_all(class_="topic")
-    for topics in topics:
-        print(topics.text)
-else:
-    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    titles = []
+    links = []
+
+    # Create a dictionary with keys to an array of topics, the keys will be the course names
+    classTopics = {}
+
+    # Check if the request was successful
+    if classResponse.status_code == 200:
+        # Parse the content of the page with BeautifulSoup
+        soup = BeautifulSoup(classResponse.content, 'html.parser')
+
+        courses = soup.find_all(class_="number-title")
+
+        for courses in courses:
+            titles.append(courses.text)
+            links.append(courses.find('a').get('href'))
+            # print(courses.text)
+            # print(courses.find('a').get('href'))
+
+    # for titles in titles:
+    #     print(titles)
+
+    for titles, links in zip(titles, links):
+
+        # print(titles + "\n\n\n")
+        topicUrl = dynamicCourseUrl + links
+
+        topicResponse = requests.get(topicUrl)
+        topics = []
+
+        if topicResponse.status_code == 200:
+            soup = BeautifulSoup(topicResponse.content, 'html.parser')
+            topicsSoup = soup.find_all(class_="topic")
+            for topicsSoup in topicsSoup:
+                topics.append(topicsSoup.text)
+                # print(topicsSoup.text)
+            classTopics[titles] = topics
+    return classTopics
+    # print(classTopics)
+
+# # URL to scrape
+# url = "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/courseInfo?courseid=716&show=true&type=undergrad"
+#
+# # Send a GET request to the URL
+# response = requests.get(url)
+#
+# # Check if the request was successful
+# if response.status_code == 200:
+#     # Parse the content of the page with BeautifulSoup
+#     soup = BeautifulSoup(response.content, 'html.parser')
+#
+#     # This shows most of the stuff I think
+#     #print(soup.prettify())
+#
+#     # Gets some of the schedule tags
+#     topics = soup.find_all(class_="topic")
+#     for topics in topics:
+#         print(topics.text)
+# else:
+#     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
