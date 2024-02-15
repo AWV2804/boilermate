597ccdc4bdf180b4d6afec210f22a2b651ab4a91
Matthew Li <mli007@hawaiiantel.net>
2024-02-15 17:27:01 -0500

    fixed firebase warnings

--- client/firebase.js
+++ client/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from 'firebase/auth';
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { ReactNativeAsyncStorage } from '@react-native-async-storage/async-storage';
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,6 +24,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
 
-export { auth };
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { app, auth, getAuth, getApp, analytics };
