f74cfa65dcb036d979a06c98eada0f99dc29d5f8
Matthew Li <mli007@hawaiiantel.net>
2024-02-16 17:29:48 -0500

    wrote firebase youtube api to process youtube videos to frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import firebase_admin
+from firebase_admin import credentials
+
+def getPath():
+    return Path(r'/mnt/c/Users/mli00/Desktop/Purdue/ECE 49595O/Boilermate-b3fcd-firebase-adminsdk-rwh4i-30e3b04f5c.json') # subject to change
+
+path = getPath()
+cred = credentials.Certificate(path)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://boilermate-b3fcd-default-rtdb.firebaseio.com'
+})
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +31,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j8-)xa3(@+u$pbzu4)og$w&^5^s_-@xip16^%=dy1b5j5)gelr'
+SECRET_KEY = 'django-insecure-)($s&&en#j9+9r^mv5qq)hb)h&+@i(4-2tk_zn64@h())wcku8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['http://127.0.0.1:8000/', 'http://127.0.0.1:8000/firebase/parse-and-send', 'http://127.0.0.1:8000/firebase/call-api-view', 'http://127.0.0.1:8000/admin/']
+
+ALLOWED_HOSTS = [] # add firebase in here later
 
 
 # Application definition
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_THROTTLE_CLASSES': [
+#         'boilermate_app.throttling.FirebaseThrottle',
+#     ],
+#     'DEFAULT_THROTTLE_RATES': {
+#         'firebase_request': '150/second',
+#     }
+# }
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +59,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.core.management',
+    'rest_framework',
+    'firebase_api',
 ]
 
 MIDDLEWARE = [
--- backend/firebase_api/views.py
+++ backend/firebase_api/views.py
@@ -1,3 +1,76 @@
+from django.http import HttpResponse
+import requests
+from backend.settings import getPath
+from pathlib import Path
+from django.http import JsonResponse
 from django.shortcuts import render
+from backend.settings import getPath
+import firebase_admin.auth
+from firebase_admin import db
+from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+class YoutubeVideoView(APIView):
+    def scrape_youtube_videos(topic):
+            api_key = 'AIzaSyBCWCmdRqhjI6LcZdwNtQEKbBqgbl18eqU'
+            url = f'https://www.googleapis.com/youtube/v3/search?key={api_key}&q={topic}&part=snippet&maxResults=10&type=video'
+            response = requests.get(url)
+            data = response.json()
+            videos = [{'title': item['snippet']['title'], 'videoId': item['id']['videoId']} for item in data['items']]
+            return videos
+        
+    def get(self, request, class_name, topic_id):
+        topic = FirebaseHandler.fetch_topics(class_name, topic_id)
+        videos = self.scrape_youtube_videos(topic)
+        return Response({'videos': videos})
+
+class FirebaseHandler(View):
+    def save_to_firebase(self, department, class_name, topic):
+        try:
+            ref = db.reference('Classes')
+            class_name = class_name.replace('/', '\u2215')
+            updated_topic = ref.child(department).child(class_name).get()
+            if updated_topic is None:
+                ref.child(department).child(class_name).set(topic)
+            else:
+                updated_topic = ', ' + topic
+                ref.child(department).child(class_name).set(updated_topic)
+            
+            saved_data = ref.child(class_name).get()
+            if saved_data['topic'] == topic:
+                return True, 'Topic saved successfully'
+            else:
+                return False, 'Failed to save topic'
+        except Exception as e:
+            return False, str(e)
+        
+            
+    def post(self, request):
+        department = request.POST.get('department')
+        class_name = request.POST.get('class_name')
+        topic = request.POST.get('topic')
+        
+        success, message = self.save_to_firebase(department, class_name, topic)
+        
+        if success:
+            return JsonResponse({'message': message}, status=201)
+        else:
+            return JsonResponse({'message': message}, status=500)
+        
+    def get(self, class_name):
+        try:
+            ref = db.reference('Classes')
+            data = ref.child(class_name).get()
+            if data:
+                return True, data
+            else:
+                return False, 'Class not found'
+        except Exception as e:
+            return False, str(e)
+        
+    def fetch_topics(class_name, topic_id):
+        ref = db.reference(f'/Classes/{class_name}/{topic_id}')
+        return ref.get()
+    
